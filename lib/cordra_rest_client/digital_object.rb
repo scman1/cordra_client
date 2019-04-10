fa265e3eeda5af1de1c541617f571f42e10f3e67
Abraham Nieva <nievadelahidalgaa@cardiff.ac.uk>
2019-04-10 12:15:25 +0100

    get object schema

--- lib/cordra_rest_client/digital_object.rb
+++ lib/cordra_rest_client/digital_object.rb
@@ -6,147 +6,137 @@ API_URL = "http://nsidr.org:8080/"
 API_HANDLE = "http://hdl.handle.net/"
 
 module CordraRestClient
-  class DigitalObject
-    # some of the attributes for digital specimen objects
-    attr_reader :id, :timestamp, :creator, :scientificName, :country, :locality,
-    	:decimalLatLong, :recordedBy, :collectionDate, :catalogNumber, 
-		:otherCatalogNumbers, :collectionCode, :institutionCode, :stableIdentifier, 
-		:physicalSpecimenId, :determinations
+	class DigitalObject
+		# digital object attributes 
+		attr_reader :id, :type, :content, :metadata
+			
+		#initialize the digital object
+		def initialize(digital_object)
+			@id = digital_object["id"]
+			@type = digital_object["type"]
+			@content = digital_object["content"]
+			@metadata = digital_object["metadata"]
+		end
 		
-	#initialize the digital specimen
-    def initialize(attributes)
-      @id = attributes["id"]
-      @timestamp = attributes["timestamp"]
-      @creator = attributes["creator"]
-      @scientificName = attributes["scientificName"]
-      @country = attributes["country"]
-      @locality = attributes["locality"]
-      @decimalLatLong = attributes["decimalLat/Long"]
-	  @recordedBy = attributes["recordedBy"]
-	  @collectionDate = attributes["collectionDate"]
-	  @catalogNumber = attributes["catalogNumber"]
-	  @otherCatalogNumbers = attributes["otherCatalogNumbers"]
-	  @collectionCode = attributes["collectionCode"]
-	  @institutionCode = attributes["institutionCode"]
-	  @stableIdentifier = attributes["stableIdentifier"]
-	  @physicalSpecimenId = attributes["physicalSpecimenId"]
-	  @determinations = attributes["Determinations"]
-    end
-	
-    # retrieves an object by ID
-    # id: id of the object to retrieve 
-    def self.find(id)
-        response = Faraday.get("#{API_URL}objects/#{id}")
-        attributes = JSON.parse(response.body)
-	DigitalObject.new(attributes)
-    end
-  
-    # retrieves an objects attribute by object ID
-    # id: id of the object to retrieve 
-    # field: name of attribute to retrieve
-    def self.get_do_field(id, field)
-	response = Faraday.get("#{API_URL}objects/#{id}?jsonPointer=/#{field}")
-	attributes = JSON.parse(response.body)
-    end
-
-    # create an object by type
-        # id: suffix of the object to create 
-	# do_type: type of digital object
-	# do_data: data of the digital object
-	# credentials: username and password for authentication
-    def self.create(id, do_type, do_data, credentials)
-        conn = Faraday.new(:url => API_URL)
-	conn.basic_auth(credentials["username"], credentials["password"])
+		# retrieves an object by ID
+		# id: id of the object to retrieve 
+		def self.find(id)
+			response = Faraday.get("#{API_URL}objects/#{id}?full=true")
+			digital_object = JSON.parse(response.body)
+			DigitalObject.new(digital_object )
+		end
 	  
-        response = conn.post do |req|
-		req.url "/objects/?type=#{do_type}&suffix=#{id}"
-	        req.headers['Content-Type'] = 'text/json'
-	        req.body = do_data.to_json
-	end
-	out = JSON.parse(response.body)
-	out [:code] = response.status
-        if response.status == 200
-                out["message"] = "OK"	  
-	end
-	return out
-    end
-	
-	# update an object by ID
-	# id: full id (prefix and suffix) of the object to update 
-	# do_data: data of the digital object
-	# credentials: username and password for authentication
-	def self.update(id, do_data, credentials)
-	  conn = Faraday.new(:url => API_URL)
+		# retrieves an objects attribute by object ID
+		# id: id of the object to retrieve 
+		# field: name of attribute to retrieve
+		def self.get_do_field(id, field)
+			response = Faraday.get("#{API_URL}objects/#{id}?jsonPointer=/#{field}")
+			attributes = JSON.parse(response.body)
+		end
 
-	  conn.basic_auth(credentials["username"], credentials["password"])
-	  response = conn.put do |req|
-	    req.url "/objects/#{id}"
-	    req.headers['Content-Type'] = 'text/json'
-	    req.body = do_data.to_json
-	  end
-	  out = JSON.parse(response.body)
-	  out [:code] = response.status
-      if response.status == 200
-        out["message"] = "OK"	  
-	  end
-	  return out  
-	end
-	
-	# delete an object
-	# id: full id (prefix and suffix) of the object to delete 
-	# credentials: username and password for authentication
-	def self.delete(id, credentials)
-	  conn = Faraday.new(:url => API_URL)
+		# create an object by type
+		# id: suffix of the object to create 
+		# do_type: type of digital object
+		# do_data: data of the digital object
+		# credentials: username and password for authentication
+		def self.create(id, do_type, do_data, credentials)
+			conn = Faraday.new(:url => API_URL)
+			conn.basic_auth(credentials["username"], credentials["password"])
+		  
+			response = conn.post do |req|
+				req.url "/objects/?type=#{do_type}&suffix=#{id}"
+				req.headers['Content-Type'] = 'text/json'
+				req.body = do_data.to_json
+			end
+			out = JSON.parse(response.body)
+			out [:code] = response.status
+			if response.status == 200
+				out["message"] = "OK"	  
+			end
+			return out
+		end
+		
+		# update an object by ID
+		# id: full id (prefix and suffix) of the object to update 
+		# do_data: data of the digital object
+		# credentials: username and password for authentication
+		def self.update(id, do_data, credentials)
+			conn = Faraday.new(:url => API_URL)
 
-	  conn.basic_auth(credentials["username"], credentials["password"])
-	  response = conn.delete do |req|
-	    req.url "/objects/#{id}"
-	  end
-	  out = JSON.parse(response.body)
-	  out [:code] = response.status
-      if response.status == 200
-        out["message"] = "OK"	  
-	  end
-	  return out  
-	end
-	
-	# search for objects
-	# do_data: data of the digital object
-	# pageNum: page number to retrieve
-	# pageSize: number of records per page to retrieve
-	def self.search(do_type, pageNum = 1, pageSize =10)
-      response = Faraday.get("#{API_URL}objects/?query=type:\"#{do_type}\"&pageNum=#{pageNum}&pageSize=#{pageSize}")
-      results = JSON.parse(response.body)	  
-	end
-	
-	# retrieves an object via the Handle System web proxy
-    # id: full id (prefix and suffix) of the object to look-up 	
-	def self.handle_find(id)
-      response = Faraday.get("#{API_HANDLE}#{id}")
-	  #the body contains the re-direction
-      response.body
-    end
-	
-	# modify the ACLs for an object
-	# allows modifying the read/write persmissions of a specific object
-	# id: id of the object to set persmissions on
-	# rw_data: two arrays containing ids of users getting r/w persmissions
-	def self.set_premissions(id,rw_data, credentials)
-	  conn = Faraday.new(:url => API_URL)
+			conn.basic_auth(credentials["username"], credentials["password"])
+			response = conn.put do |req|
+				req.url "/objects/#{id}"
+				req.headers['Content-Type'] = 'text/json'
+				req.body = do_data.to_json
+			end
+			out = JSON.parse(response.body)
+			out [:code] = response.status
+			if response.status == 200
+				out["message"] = "OK"	  
+			end
+			return out  
+		end
+		
+		# delete an object
+		# id: full id (prefix and suffix) of the object to delete 
+		# credentials: username and password for authentication
+		def self.delete(id, credentials)
+			conn = Faraday.new(:url => API_URL)
 
-	  conn.basic_auth(credentials["username"], credentials["password"])
-	  response = conn.put do |req|
-	    req.url "/acls/#{id}"
-	    req.headers['Content-Type'] = 'text/json'
-	    req.body = rw_data.to_json
-	  end
-	  out = JSON.parse(response.body)
-	  out [:code] = response.status
-      if response.status == 200
-        out["message"] = "OK"	  
-	  end
-	  return out
+			conn.basic_auth(credentials["username"], credentials["password"])
+			response = conn.delete do |req|
+				req.url "/objects/#{id}"
+			end
+			out = JSON.parse(response.body)
+			out [:code] = response.status
+			if response.status == 200
+				out["message"] = "OK"	  
+			end
+			return out  
+		end
+		
+		# search for objects
+		# do_data: data of the digital object
+		# pageNum: page number to retrieve
+		# pageSize: number of records per page to retrieve
+		def self.search(do_type, pageNum = 1, pageSize =10)
+			response = Faraday.get("#{API_URL}objects/?query=type:\"#{do_type}\"&pageNum=#{pageNum}&pageSize=#{pageSize}")
+			results = JSON.parse(response.body)	  
+		end
+		
+		# retrieves an object via the Handle System web proxy
+	    # id: full id (prefix and suffix) of the object to look-up 	
+		def self.handle_find(id)
+			response = Faraday.get("#{API_HANDLE}#{id}")
+			#the body contains the re-direction
+		       response.body
+		end
+		
+		# modify the ACLs for an object
+		# allows modifying the read/write persmissions of a specific object
+		# id: id of the object to set persmissions on
+		# rw_data: two arrays containing ids of users getting r/w persmissions
+		def self.set_premissions(id,rw_data, credentials)
+			conn = Faraday.new(:url => API_URL)
+
+			conn.basic_auth(credentials["username"], credentials["password"])
+			response = conn.put do |req|
+				req.url "/acls/#{id}"
+				req.headers['Content-Type'] = 'text/json'
+				req.body = rw_data.to_json
+			end
+			out = JSON.parse(response.body)
+			out [:code] = response.status
+			if response.status == 200
+				out["message"] = "OK"	  
+			end
+			return out
+		end
+		# retrieve a schema
+		# schema_type: the schema to retrieve, if empty returns all schemas
+		def self.get_schema(schema_type="")
+			conn= Faraday.get("#{API_URL}schemas/#{schema_type}")
+		end
 	end
-  end
 end
 
--- test/digital_object/digital_object_test.rb
+++ test/digital_object/digital_object_test.rb
@@ -2,139 +2,146 @@
 require './test/test_helper'
 
 class CordraRestClientDigitalObjectTest < Minitest::Test
-  # basic test 
-  def test_exists
-    assert CordraRestClient::DigitalObject
-  end
-  
-  # test retrieve object by ID
-  def test_retrieve_object_by_id
-    VCR.use_cassette('retrieve_object_id') do
-      cdo = CordraRestClient::DigitalObject.find("20.5000.1025/B100003484")
-      assert_equal CordraRestClient::DigitalObject, cdo.class
+	# basic test 
+	def test_exists
+		assert CordraRestClient::DigitalObject
+	end
 	  
-      # Check that fields are accessible
-      assert_equal "20.5000.1025/B100003484", cdo.id
-	  assert_nil   cdo.timestamp
-	  assert_equal "20.5000.1025/60c6d277a8bd81de7fdd", cdo.creator
-      assert_equal "Agathis palmerstonii (F.Muell.) F.M.Bailey", cdo.scientificName
-      assert_equal "Australia", cdo.country
-      assert_equal "Queensland", cdo.locality
-      assert_nil   cdo.decimalLatLong
-	  assert_equal "L. J. Brass 2421", cdo.recordedBy
-      assert_equal "1932-04-08", cdo.collectionDate
-      assert_equal "B 10 0003484", cdo.catalogNumber
-      assert_equal "2421", cdo.otherCatalogNumbers
-      assert_equal "Australia: Queensland", cdo.collectionCode
-      assert_equal "B", cdo.institutionCode
-      assert_equal "http://herbarium.bgbm.org/object/B100003484", cdo.stableIdentifier
-      assert_equal "B 10 0003484", cdo.physicalSpecimenId
-      assert_equal "Yes", cdo.determinations
-    end
-  end
-  # test retrieve object creator, variant of  retrieve object by ID
-  def test_retrieve_object_creator
-    VCR.use_cassette('retrieve_object_attribute') do
-	do_creator = CordraRestClient::DigitalObject.get_do_field("20.5000.1025/B100003484","creator")
-        # Check object creator
-        assert_equal "20.5000.1025/60c6d277a8bd81de7fdd", do_creator
-    end
-  end
-  # pending testing of get payload, other attributes
-  
-  # test create an object by type
-  def test_create_object_by_type
-    VCR.use_cassette('create_object') do
-	  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
-      json = JSON.parse(File.read("test/fixtures/new_specimen.json"))
-
-	  result=CordraRestClient::DigitalObject.create("RMNH.RENA.38646","Digital Specimen",json, cred["uc_1"])
-      
-	  #check that the result is saved
-	  assert_equal 200, result[:code]
-	  assert_equal "OK", result["message"]
-	end
-  end
-  
-  # test create an object by type, FAIL
-  def test_create_object_by_type_fail
-    VCR.use_cassette('create_object_fail') do
-	  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
-
-      json = JSON.parse(File.read("test/fixtures/new_specimen.json"))
-	
-	  result=CordraRestClient::DigitalObject.create("1a0beb212baaede1c10c","Digital Specimen",json, cred["uc_1"])
-	  #check that the duplicate is rejected
-	  assert_equal 409, result[:code]
-	  assert_equal "Object already exists: 20.5000.1025/1a0beb212baaede1c10c", result["message"]
-	end
-  end
-  
-  # test update an object by ID
-  def test_update_object_by_id
-    VCR.use_cassette('edit_object') do
-	  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
-	  json = JSON.parse(File.read("test/fixtures/edit_specimen.json"))
-	  id = json["id"] 
-	  json["id"] = "" #id cannot be updated
-	  result=CordraRestClient::DigitalObject.update(id, json, cred["uc_1"])
-
-	  #check that the result is saved
-	  assert_equal 200, result[:code]
-	  assert_equal "OK", result["message"]
-	end
-  end
-  
-  # test delete an object by ID
-  def test_delete_object_by_id
-    VCR.use_cassette('delete_object') do
-	  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
-	  id = "20.5000.1025/newspecimen03" 
-	  result=CordraRestClient::DigitalObject.delete(id, cred["uc_1"])
-
-	  #check that the result is saved
-	  assert_equal 200, result[:code]
-	  assert_equal "OK", result["message"]
-	end
-  end
-  
-  # test search for objects
-  def test_search_for_objects
-    VCR.use_cassette('search_objects') do
-      list_cdo = CordraRestClient::DigitalObject.search("Digital Specimen")
-      assert_equal Hash, list_cdo.class
+	# test retrieve object by ID
+	def test_retrieve_object_by_id
+		VCR.use_cassette('retrieve_object_id') do
+			cdo = CordraRestClient::DigitalObject.find("20.5000.1025/B100003484")
+			assert_equal CordraRestClient::DigitalObject, cdo.class
+			  
+			# Check that fields are accessible
+			assert_equal "20.5000.1025/B100003484", cdo.id
+			assert_equal "Digital Specimen", cdo.type
+			puts("\n*********************************************")
+			puts(cdo.content)
+			puts(cdo.metadata)
+			puts("\n*********************************************")
+		end
+	  end
+	  # test retrieve object creator, variant of  retrieve object by ID
+	def test_retrieve_object_creator
+		VCR.use_cassette('retrieve_object_attribute') do
+			do_creator = CordraRestClient::DigitalObject.get_do_field("20.5000.1025/B100003484","creator")
+			# Check object creator
+		 	assert_equal "20.5000.1025/60c6d277a8bd81de7fdd", do_creator
+		end
+	 end
+	  # pending testing of get payload, other attributes
 	  
-      # Check that fields are accessible
-      assert_equal 1, list_cdo["pageNum"]
-	  assert_equal 10, list_cdo["pageSize"]
-	  assert_equal 24, list_cdo["size"]
-	  assert_equal Array, list_cdo["results"].class
-    end
-  end
-  # test retrieves an object via the Handle System web proxy
-  def test_retrieve_objects_via_handle
-    VCR.use_cassette('get_object_hanlde') do
-      redirection = CordraRestClient::DigitalObject.handle_find("20.5000.1025/B100003484")
-      assert_equal String, redirection.class
-	  
-      # Check that fields are accessible
-	  assert_match /Handle Redirect/, redirection
-	  assert_match /B100003484/, redirection
-    end
-  end  
-  # test modify the ACLs (permissions) for a specific object 
-  # Not working, could be because of limits of test user credentials
-  def test_modify_object_permissions
-    VCR.use_cassette('modify_permissions') do
-	  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
-	  json = JSON.parse(File.read("test/fixtures/acl_list.json"))
-	  id = "20.5000.1025/RMNH.RENA.44084" 
-	  result=CordraRestClient::DigitalObject.set_premissions(id, json, cred["uc_1"])
+	  # test create an object by type
+	  def test_create_object_by_type
+	    VCR.use_cassette('create_object') do
+		  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
+	      json = JSON.parse(File.read("test/fixtures/new_specimen.json"))
 
-	  #check that the result is saved
-	  assert_equal 200, result[:code]
-	  assert_equal "OK", result["message"]
+		  result=CordraRestClient::DigitalObject.create("RMNH.RENA.38646","Digital Specimen",json, cred["uc_1"])
+	      
+		  #check that the result is saved
+		  assert_equal 200, result[:code]
+		  assert_equal "OK", result["message"]
+		end
+	  end
+	  
+	  # test create an object by type, FAIL
+	  def test_create_object_by_type_fail
+	    VCR.use_cassette('create_object_fail') do
+		  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
+
+	      json = JSON.parse(File.read("test/fixtures/new_specimen.json"))
+		
+		  result=CordraRestClient::DigitalObject.create("1a0beb212baaede1c10c","Digital Specimen",json, cred["uc_1"])
+		  #check that the duplicate is rejected
+		  assert_equal 409, result[:code]
+		  assert_equal "Object already exists: 20.5000.1025/1a0beb212baaede1c10c", result["message"]
+		end
+	  end
+	  
+	  # test update an object by ID
+	  def test_update_object_by_id
+	    VCR.use_cassette('edit_object') do
+		  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
+		  json = JSON.parse(File.read("test/fixtures/edit_specimen.json"))
+		  id = json["id"] 
+		  json["id"] = "" #id cannot be updated
+		  result=CordraRestClient::DigitalObject.update(id, json, cred["uc_1"])
+
+		  #check that the result is saved
+		  assert_equal 200, result[:code]
+		  assert_equal "OK", result["message"]
+		end
+	  end
+	  
+	  # test delete an object by ID
+	  def test_delete_object_by_id
+	    VCR.use_cassette('delete_object') do
+		  cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
+		  id = "20.5000.1025/newspecimen03" 
+		  result=CordraRestClient::DigitalObject.delete(id, cred["uc_1"])
+
+		  #check that the result is saved
+		  assert_equal 200, result[:code]
+		  assert_equal "OK", result["message"]
+		end
+	  end
+	  
+	  # test search for objects
+	  def test_search_for_objects
+	    VCR.use_cassette('search_objects') do
+	      list_cdo = CordraRestClient::DigitalObject.search("Digital Specimen")
+	      assert_equal Hash, list_cdo.class
+		  
+	      # Check that fields are accessible
+	      assert_equal 1, list_cdo["pageNum"]
+		  assert_equal 10, list_cdo["pageSize"]
+		  assert_equal 24, list_cdo["size"]
+		  assert_equal Array, list_cdo["results"].class
+	    end
+	  end
+	  # test retrieves an object via the Handle System web proxy
+	  def test_retrieve_objects_via_handle
+	    VCR.use_cassette('get_object_hanlde') do
+	      redirection = CordraRestClient::DigitalObject.handle_find("20.5000.1025/B100003484")
+	      assert_equal String, redirection.class
+		  
+	      # Check that fields are accessible
+		  assert_match /Handle Redirect/, redirection
+		  assert_match /B100003484/, redirection
+	    end
+	end  
+    
+	# test modify the ACLs (permissions) for a specific object 
+	# Not working, could be because of limits of test user credentials
+	def test_modify_object_permissions
+		VCR.use_cassette('modify_permissions') do
+			cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
+			json = JSON.parse(File.read("test/fixtures/acl_list.json"))
+			id = "20.5000.1025/RMNH.RENA.44084" 
+			result=CordraRestClient::DigitalObject.set_premissions(id, json, cred["uc_1"])
+
+			#check that the result is saved
+			assert_equal 200, result[:code]
+			assert_equal "OK", result["message"]
+		end
 	end
-  end
+
+	# test get object schema
+	# 
+	def test_modify_object_permissions
+		VCR.use_cassette('get_schema') do
+			cred=JSON.parse(YAML::load_file('test/fixtures/credential.yml').to_json)
+			json = JSON.parse(File.read("test/fixtures/acl_list.json"))
+			schema_type="Digital%20Specimen"
+			result=CordraRestClient::DigitalObject.get_schema(schema_type)
+			do_schema = JSON.parse(result.body)
+			#check that the result is saved
+			assert_equal "object", do_schema["type"] 
+			assert_equal "Digital Specimen", do_schema["title"]
+		end
+	end
+
 end
 
